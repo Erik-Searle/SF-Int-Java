42692888e51e0a38535fc9de094e0e66aa646e88
Simon Roberts <simon@dancingcloudservices.com>
2021-01-11 11:48:58 -0700

    Lab 1 definition, students.Student

---
+++ src/main/java/students/Student.java
@@ -0,0 +1,14 @@
+package students;
+
+public class Student {
+  /*
+  Business domain requirements for a student:
+   - each has a name
+   - an overall gpa (0-> 4.0 range)
+   - names of courses being taken
+
+   "School"..
+    - "several" students
+    - print out all the "smart" students (you decide what "smart" means).
+   */
+}
