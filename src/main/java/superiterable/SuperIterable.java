26911212a11c466e31ec6e1e12014528ab07efd5
Simon Roberts <simon@dancingcloudservices.com>
2021-01-12 12:26:59 -0700

    Day 2 12:26 Lab

--- src/main/java/superiterable/SuperIterable.java
+++ src/main/java/superiterable/SuperIterable.java
@@ -5,6 +5,7 @@ import students.Student;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class SuperIterable<E> implements Iterable<E> {
@@ -12,6 +13,14 @@ public class SuperIterable<E> implements Iterable<E> {
 
   public SuperIterable(Iterable<E> self) {
     this.self = self;
+  }
+
+  public <F> SuperIterable<F> map(Function<E, F> op) {
+    List<F> out = new ArrayList<>();
+    for (E e : self) {
+      out.add(op.apply(e));
+    }
+    return new SuperIterable<>(out);
   }
 
   public SuperIterable<E> filter(Predicate<E> crit) {
@@ -37,6 +46,22 @@ public class SuperIterable<E> implements Iterable<E> {
 
     for (String s : sis) {
       System.out.println("> " + s);
+
+      System.out.println("-------------------");
+      sis
+          .filter(w -> w.length() > 3)
+//          .map(w -> w.toUpperCase())
+          .map(String::toUpperCase)
+          .forEach(System.out::println);
+
+      System.out.println("-------------------");
+      sis
+          .filter(w -> w.length() > 3)
+          .map(w -> {
+            System.out.println("> " + w);
+            return null;
+          })
+      .forEach(System.out::println);
     }
   }
 }
